5d613a8dffa72b2cc6f78df7bb9a57f927238c7d
MehediHasan4321 <126mehedi@gmail.com>
2023-05-08 16:47:58 +0600

    update delete method has been implement complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -25,20 +25,53 @@ async function run() {
   try {
     const coffeeCollection = client.db('coffeeDB').collection('coffee')
 
-    app.get('/coffees',async(req,res)=>{
-        const cursor = coffeeCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    app.get('/coffees', async (req, res) => {
+      const cursor = coffeeCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
     })
 
-    app.post('/coffees',async(req,res)=>{
-        const newCoffee = req.body
-        console.log(newCoffee)
-        const result = await coffeeCollection.insertOne(newCoffee)
-        res.send(result)
+    app.get('/coffees/:id', async (req, res) => {
+      const id = req.params.id
+      const quire = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.findOne(quire)
+      res.send(result)
     })
 
+    app.post('/coffees', async (req, res) => {
+      const newCoffee = req.body
+      console.log(newCoffee)
+      const result = await coffeeCollection.insertOne(newCoffee)
+      res.send(result)
+    })
 
+    app.delete('/coffees/:id', async (req, res) => {
+      const id = req.params.id
+      const quire = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.deleteOne(quire)
+      res.send(result)
+    })
+
+    app.put('/coffees/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const option = { upsert: true }
+      const updateCoffee = req.body
+      const coffee = {
+        $set: {
+          coffeeName: updateCoffee.chefName,
+          chefName: updateCoffee.chefName,
+          supplyerName: updateCoffee.supplyerName,
+          test: updateCoffee.test,
+          category: updateCoffee.category,
+          details: updateCoffee.details,
+          url: updateCoffee.url
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter, coffee, option)
+      res.send(result)
+
+    })
 
 
     // Connect the client to the server	(optional starting in v4.7)
@@ -54,11 +87,11 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-    res.send('Coffee Shop Server is Running')
+app.get('/', (req, res) => {
+  res.send('Coffee Shop Server is Running')
 })
 
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
